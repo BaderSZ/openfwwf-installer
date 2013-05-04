62ee55bfa0d26b861e59943f9df5dc118d2b8f71
BaderSZ <bader.zaidan@gmail.com>
2013-05-05 00:15:20 +0200

    added comments

--- b4331.sh
+++ b4331.sh
@@ -7,6 +7,7 @@ FWDIR="./firmware"
 FWDIR_PROP="./firmware/proprietary"
 FW_INSTALL_DIR="/lib/firmware/b43-open/"
 
+
 echo "Accessing apt-get to install the neccessary utilities"
 
 sudo apt-get install flex bison debhelper wget build-essential make binutils
@@ -14,7 +15,8 @@ sudo apt-get install flex bison debhelper wget build-essential make binutils
 mkdir $FWDIR
 cd $FWDIR
 
-echo "Downloading needed packages, from the interwebz"
+
+echo "Downloading needed packages, from the internet..."
 
 wget     http://bues.ch/b43/fwcutter/b43-fwcutter-017.tar.bz2
 wget     http://www.lwfinger.com/b43-firmware/broadcom-wl-5.100.138.tar.bz2
@@ -24,16 +26,24 @@ wget     http://mirror.fsf.org/trisquel/pool/main/b/b43-asm/b43-asm_0~20080619-0
 tar xjf b43-fwcutter-017.tar.bz2
 tar xjf broadcom-wl-5.100.138.tar.bz2
 
+
+## Now that installation and extraction is done, install b43-fwcutter.
 cd b43-fwcutter-017
 make
 sudo make install
 cd ..
 
+
+## openfwwf depends on b43-asm, install both
 sudo dpkg -i b43-asm_0~20080619-0+c1.aptosid.9trisquel1_amd64.deb
 sudo dpkg -i openfwwf_5.2-0trisquel3_all.deb
 
+
+## extract the proprietary firmware to the temporary directory.
 sudo b43-fwcutter -w $FWDIR_PROP broadcom-wl-5.100.138/linux/wl_apsta.o
 
+
+## copy only the needed proprietary files. To the firmware directory.
 sudo cp $FWDIR_PROP/ht0initvals29.fw    $FW_INSTALL_DIR
 sudo cp $FWDIR_PROP/ht0bsinitvals29.fw  $FW_INSTALL_DIR
 sudo cp $FWDIR_PROP/ucode29_mimo.fw     $FW_INSTALL_DIR
@@ -41,5 +51,4 @@ sudo cp $FWDIR_PROP/ucode29_mimo.fw     $FW_INSTALL_DIR
 
 echo "-------------------------"
 echo "Done! Please reboot the computer..."
-
 
